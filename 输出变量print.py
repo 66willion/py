f1475b3e4d0da14e1301587509d84be24563a669
66willion <122015580+66willion@users.noreply.github.com>
2023-09-04 21:05:39 +0800

    Update 输出变量print.py

--- 输出变量print.py
+++ 输出变量print.py
@@ -1,2 +1,11 @@
 #可以输出数字
-print
+print(520)
+#输出字符串时要加引号
+print('hello world')
+#将数据输出到文件中
+#a是读写的方式创建文档，+是如果没有就创建新的，fp是变量
+fp=open('D:/text.txt',a+)
+#将hello world输入到fp文件中
+print('hello world',file=fp)
+#关闭fp文件
+fp.close()
